ae2f73b4a536cd60e3f957372a1463b097a4ca55
nabilahazmi2 <126212994+nabilahazmi2@users.noreply.github.com>
2023-11-10 12:24:19 +0800

    Add files via upload

---
+++ bigcube-ordering-system/components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:cikzah123@gmail.com">cikzah123@gmail.com</a>
+         <a href="mailto:ayoi123@gmail.com">ayoi123@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>08:30am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">BIGCUBE KPM BERANANG</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">011-2890989</a>
+         <a href="tel:1112223333">019-9087690</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>NABILAH DESIGN</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
